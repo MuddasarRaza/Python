7e6d9b42cbab4f064c2e5aa03b2d2b4aed8cfdae
Syed Muddasar Raza <muddasar.raza@kardex.com>
2019-05-27 13:27:52 +0200

    remegrsda

--- mqttv1.py
+++ mqttv1.py
@@ -84,3 +84,43 @@ if __name__ == '__main__':::::::::::::::::::::
     finally:
         if conn is not None:
             conn.open()
+
+
+    create_tables()
+    #######################################
+        # read the connection parameters
+        params = config()
+        # connect to the PostgreSQL server
+        conn = psycopg2.connect(host="192.168.10.50", database="datalogdb", user="postgres", password="dbPassword")
+        cur = conn.cursor()
+        # create table one by one
+        for command in commands:
+            cur.execute(command)
+        # close communication with the PostgreSQL database server
+        cur.close()
+        # commit the changes
+        conn.commit()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.open()
+    create_tables()
+    ###############################################################################
+        # read the connection parameters
+        params = config()
+        # connect to the PostgreSQL server
+        conn = psycopg2.connect(host="192.168.10.50", database="datalogdb", user="postgres", password="dbPassword")
+        cur = conn.cursor()
+        # create table one by one
+        for command in commands:
+            cur.execute(command)
+        # close communication with the PostgreSQL database server
+        cur.close()
+        # commit the changes
+        conn.commit()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.open()
